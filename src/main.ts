ab550a7aac43b3a5d5869b0d46d57a76547359eb
Hanniel21 <dhanniel21@gmail.com>
2026-08-19 12:53:16 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { corsOptions } from './cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors(corsOptions);
+  app.enableCors({
+    origin: [
+      'https://hems-ui.vercel.app', 
+      'http://localhost:3000', 
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 5000);
 }
 void bootstrap();
