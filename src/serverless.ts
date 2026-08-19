23e05422a83ac180e49c6fe5026c7a4b51b3aac2
Hanniel21 <dhanniel21@gmail.com>
2026-08-19 12:59:28 +0100

    cors

--- src/serverless.ts
+++ src/serverless.ts
@@ -4,7 +4,6 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 import { AppModule } from './app.module';
-import { corsOptions } from './cors.config';
 
 let cachedApp: express.Express | null = null;
 
@@ -15,7 +14,14 @@ export async function getExpressApp(): Promise<express.Express> {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressApp),
-    { cors: corsOptions },
+    {
+      cors: {
+        origin: ['https://hems-ui.vercel.app', 'http://localhost:3000'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        allowedHeaders: 'Content-Type, Accept, Authorization',
+        credentials: true,
+      },
+    },
   );
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.init();
